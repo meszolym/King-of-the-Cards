ba5d03716b0f70be3c108d58c5000d68248c7b31
meszolym <62993044+meszolym@users.noreply.github.com>
2024-11-25 00:07:42 +0100

    Corrected shoe cards being settable

--- KC.App.Models/Classes/Shoe.cs
+++ KC.App.Models/Classes/Shoe.cs
@@ -4,7 +4,7 @@ namespace KC.App.Models.Classes;
 
 public class Shoe(List<Card> cards)
 {
-    public List<Card> Cards { get; set; } = cards;
+    public List<Card> Cards { get; } = cards;
     public int NextCardIdx; //index of the card that will be dealt next
     public int ShuffleCardIdx = -1; //invalid index
 }
