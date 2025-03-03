7b1c4b1b57d54e895c36dc37953528767ffbb7e1
meszolym <meszolym@MARCPC>
2025-03-03 17:01:17 +0100

    Started working on SessionView

---
+++ KC.App.Frontend/ViewModels/SessionBoxControlsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KC.App.Frontend.ViewModels
+{
+    class SessionBoxControlsViewModel : ReactiveObject
+    {
+
+    }
+}
---
+++ KC.App.Frontend/Views/Components/SessionBoxControlsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace KC.App.Frontend;
+
+public partial class SessionBoxControlsView : UserControl
+{
+    public SessionBoxControlsView()
+    {
+        InitializeComponent();
+    }
+}
