383c67f02f792d4eb7cf5e8e4166026bcf009ffe
Mészöly Marcell <marcell.meszoly@icloud.com>
2025-04-09 20:58:40 +0200

    Refactor BoxView to clean up commented code and improve binding logic

--- KC.Frontend.Client/Views/Components/BoxView.axaml.cs
+++ KC.Frontend.Client/Views/Components/BoxView.axaml.cs
@@ -24,12 +24,9 @@ namespace KC.Frontend.Client.Views.Components
         public BoxView()
         {
             InitializeComponent();
-            //ViewModel = new BoxViewModel();
             
             this.WhenActivated(d =>
             {
-                // ViewModel.WhenAnyValue(vm => vm.IsSplit).Subscribe(UpdateColumnDefinitions).DisposeWith(d);
-                
                 // Bind the right hand view model
                 this.OneWayBind(ViewModel, 
                         vm => vm.RightHand, 
@@ -42,26 +39,12 @@ namespace KC.Frontend.Client.Views.Components
                     vm => vm.LeftHand, 
                     view => view.LeftHandViewFound.ViewModel)
                     .DisposeWith(d);
-                //_rightHandView.IsVisible = false;
-                // this.OneWayBind(ViewModel,
-                //     vm => vm.IsSplit,
-                //     v => v.LeftHandViewFound.IsVisible);
 
                 // Bind player name
                 this.Bind(ViewModel, 
                         vm => vm.PlayerName, 
                         v => v.PlayerNameTextBlockFound.Text)
                     .DisposeWith(d);
-                
-                // // Update appearance when player controlled
-                // this.WhenAnyValue(x => x.ViewModel.IsPlayerControlled)
-                //     .Subscribe(isPlayerControlled => UpdatePlayerControlledState(isPlayerControlled))
-                //     .DisposeWith(d);
-                
-                // Update visibility of right hand based on split state
-                // this.WhenAnyValue(x => x.ViewModel.IsSplit)
-                //     .Subscribe(isSplit => LeftHandViewFound.IsVisible = isSplit)
-                //     .DisposeWith(d);
 
                 var localPlayerGuid = Locator.Current.GetRequiredService<PlayerViewModel>().Id;
                 
@@ -69,8 +52,6 @@ namespace KC.Frontend.Client.Views.Components
                 this.OneWayBind(ViewModel, vm => vm.IsClaimed, v=> v.ClaimButtonFound.IsVisible, b => !b && ViewModel.OwnerId == Guid.Empty);
                 this.BindCommand(ViewModel, vm => vm.DisclaimBoxCommand, view => view.UnclaimButtonFound);
                 this.OneWayBind(ViewModel, vm => vm.IsClaimed, v=> v.UnclaimButtonFound.IsVisible, b => b && ViewModel.OwnerId == localPlayerGuid);
-                
-                
 
             });
         }
@@ -79,39 +60,5 @@ namespace KC.Frontend.Client.Views.Components
         {
             AvaloniaXamlLoader.Load(this);
         }
-        
-        // private void UpdateColumnDefinitions(bool isSplit)
-        // {
-        //     var grid = this.FindControl<Grid>("MainGrid"); // Add x:Name="MainGrid" to your Grid in XAML
-        //
-        //     if (isSplit)
-        //     {
-        //         // Split evenly when right hand is visible
-        //         grid.ColumnDefinitions[0].Width = new GridLength(1, GridUnitType.Star);
-        //         grid.ColumnDefinitions[1].Width = new GridLength(1, GridUnitType.Star);
-        //     }
-        //     else
-        //     {
-        //         // Left hand takes all space when right hand is hidden
-        //         grid.ColumnDefinitions[0].Width = new GridLength(1, GridUnitType.Star);
-        //         grid.ColumnDefinitions[1].Width = GridLength.Auto;
-        //     }
-        // }
-        
-        // private void UpdatePlayerControlledState(bool isPlayerControlled)
-        // {
-        //     if (isPlayerControlled)
-        //     {
-        //         // Add visual indication that this box is player-controlled
-        //         // For example, change border color or add a highlight effect
-        //         var border = this.Parent as Border;
-        //         if (border != null)
-        //         {
-        //             border.BorderBrush = new Avalonia.Media.SolidColorBrush(
-        //                 Avalonia.Media.Color.FromRgb(255, 215, 0)); // Gold color
-        //             border.BorderThickness = new Thickness(3);
-        //         }
-        //     }
-        // }
     }
 }
