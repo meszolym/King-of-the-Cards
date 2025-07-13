a755910296da22482f136ae24b65487fc558bade
meszolym <marcell.meszoly@icloud.com>
2025-07-13 14:24:16 +0200

    Add reset balance button visibility logic based on connection and player balance

--- KC.Frontend.Client/Views/MainWindow.axaml.cs
+++ KC.Frontend.Client/Views/MainWindow.axaml.cs
@@ -25,7 +25,7 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
             this.OneWayBind(ViewModel, vm => vm.ClientMacAddress, v => v.PlayerMacTextBlock.Text, mac => $"(MAC: {mac})").DisposeWith(d);
 
             this.BindCommand(ViewModel, vm => vm.ResetBalanceCommand, v => v.ResetBalanceButton).DisposeWith(d);
-            this.OneWayBind(ViewModel, vm => vm.PlayerViewModel.PlayerBalance, v => v.ResetBalanceButton.IsVisible, balance => balance == 0).DisposeWith(d);
+            this.OneWayBind(ViewModel, vm => vm.ResetBalanceEnabled, v => v.ResetBalanceButton.IsVisible).DisposeWith(d);
             
             this.BeforeFirstConnStackPanel.IsVisible = true;
             await ViewModel!.InitAsync();
