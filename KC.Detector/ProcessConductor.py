17d8200e4103c181549ea8e27d305af901ce39d2
meszolym <marcell.meszoly@icloud.com>
2025-09-28 22:33:40 +0200

    Add MessageProcessor integration and enhance threading in ProcessConductor

--- KC.Detector/ProcessConductor.py
+++ KC.Detector/ProcessConductor.py
@@ -1,3 +1,5 @@
+from rx import operators
+
 from ImageProcessing.Preprocessor import Preprocessor
 from Models.RoisContainer import RoisContainer
 from ImageProcessing.MessageProcessor import *
@@ -6,14 +8,26 @@ from Models.Enums import Message
 from Models.Card import Card
 from Models.Table import Table
 from CardCounting.Organizer import organize_players_cards, organize_dealer_cards
+from rx.scheduler import ThreadPoolScheduler
+import multiprocessing
+
 class ProcessConductor:
 
     preprocessor: Preprocessor
     table_state: Table
+    msg_processor: MessageProcessor
+    pool_scheduler : ThreadPoolScheduler
 
     def __init__(self):
         self.table_state = Table(None, None, None)
         self.preprocessor = None
+        self.pool_scheduler = ThreadPoolScheduler(multiprocessing.cpu_count())
+        return
+
+    def read_possible_messages(self, filepath: str):
+        if self.msg_processor is None:
+            self.msg_processor = MessageProcessor()
+        self.msg_processor.read_possible_messages(filepath)
         return
 
     def rois_selected_handler(self, rois : RoisContainer):
@@ -22,7 +36,11 @@ class ProcessConductor:
             return
 
         self.preprocessor = Preprocessor(rois)
-        self.preprocessor.message_image_observable.subscribe(lambda img: self.message_image_handler(img))
+        self.preprocessor.message_image_observable.pipe(
+            operators.observe_on(self.pool_scheduler)
+        ).subscribe(lambda img: self.message_image_handler(img))
+
+
         self.preprocessor.dealer_image_observable.subscribe(lambda img: self.dealer_image_handler(img))
         self.preprocessor.player_image_observable.subscribe(lambda img: self.player_image_handler(img))
         return
@@ -35,12 +53,13 @@ class ProcessConductor:
         self.preprocessor.stop()
         return
 
-    @staticmethod
     def message_image_handler(self, image):
-        msg : Message = process_message(image)
+        msg : Message = self.msg_processor.process_message(image)
         match msg:
             case Message.Shuffling:
                 print("Shuffling detected") #TODO: reset stuff
+            case Message.WaitingForBets:
+                print("Waiting for bets detected")
             #...
         # TODO: Update GUI
         return
