32bcdee4bb8b275b5053f680de673d24ab5b5c1d
meszolym <marcell.meszoly@icloud.com>
2025-09-28 22:24:13 +0200

    Implement MessageProcessor class for message extraction and add Messages.json

--- KC.Detector/ImageProcessing/MessageProcessor.py
+++ KC.Detector/ImageProcessing/MessageProcessor.py
@@ -1,4 +1,27 @@
+import pytesseract
+
+from ImageProcessing.ScreenCapturer import take_screenshot
 from Models.Enums import Message
-def process_message(img) -> Message:
-    #TODO: implement message processing
-    return Message.Unknown
+import json
+import cv2 as cv
+from PIL import Image
+
+class MessageProcessor:
+    possible_messages : dict[str, Message]
+
+    def __init__(self):
+        self.possible_messages = {}
+
+    def read_possible_messages(self, filepath: str):
+        with open(filepath) as f:
+            self.possible_messages = json.load(f)
+
+    def process_message(self, img) -> Message:
+        img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        pil_img = Image.fromarray(img_rgb)
+        extracted_text = pytesseract.image_to_string(pil_img)
+        for k,v in self.possible_messages.items():
+            if k in extracted_text:
+                return v
+
+        return Message.Unknown
