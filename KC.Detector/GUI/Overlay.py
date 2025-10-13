eeafd07fb9c3c88c4d1fd790b7763e306cc396d8
meszolym <marcell.meszoly@icloud.com>
2025-10-13 10:05:47 +0200

    Refactor Overlay class in Overlay.py to improve layout handling; switch from place() to pack() for better widget management (for debugging)

--- KC.Detector/GUI/Overlay.py
+++ KC.Detector/GUI/Overlay.py
@@ -3,7 +3,7 @@ from tkinter import *
 from screeninfo import get_monitors
 
 from Models import Enums
-from Models.OverlayData import OverlayModel
+from Models.OverlayModel import OverlayModel
 
 
 class Overlay:
@@ -20,10 +20,10 @@ class Overlay:
 
         self.window = Tk()
         self.window.overrideredirect(True)
-        self.window.attributes("-topmost", True)
-        self.window.attributes("-transparentcolor", "black")
-        monitor = get_monitors()[0]
-        self.window.geometry(f"{monitor.width}x{monitor.height}")
+        # self.window.attributes("-topmost", True)
+        # self.window.attributes("-transparentcolor", "black")
+        # monitor = get_monitors()[0]
+        # self.window.geometry(f"{monitor.width}x{monitor.height}")
         self.update_overlay()
 
 
@@ -37,18 +37,22 @@ class Overlay:
         for x, y, score, move in self.data.player_hand_info:
             move_info = Enums.Move(move).name if move != Enums.Move.Unknown else "No advice"
             text = f"{score}\n{move_info}"
+            text += f"\n{x},{y}"  # For debugging position
             player_label = Label(self.window, text=text, font=("Arial", 16), bg="black", fg="yellow")
-            player_label.place(x=x, y=y + self.CONST_Y_OFFSET)
+            # player_label.place(x=x, y=y + self.CONST_Y_OFFSET)
+            player_label.pack(pady=5)
 
         # Add label for dealer hand
         dealer_x, dealer_y, dealer_score = self.data.dealer_hand_info
         dealer_label = Label(self.window, text=f"{dealer_score}", font=("Arial", 16), bg="black", fg="yellow")
-        dealer_label.place(x=dealer_x, y=dealer_y + self.CONST_Y_OFFSET)
+        # dealer_label.place(x=dealer_x, y=dealer_y + self.CONST_Y_OFFSET)
+        dealer_label.pack(pady=5)
 
         # Add label for counts
         count_text = f"Running Count: {self.data.table_running_count}\nTrue Count: {self.data.table_true_count}"
         count_label = Label(self.window, text=count_text, font=("Arial", 16), bg="black", fg="yellow")
-        count_label.place(x=10, y=10)
+        # count_label.place(x=10, y=10)
+        count_label.pack(pady=5)
 
         return
 
